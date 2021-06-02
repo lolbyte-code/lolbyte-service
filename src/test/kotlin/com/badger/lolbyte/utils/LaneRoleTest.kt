76a598046ac7774dc3cffa626cab509c851a55f5
Anthony Schott <acschott526@gmail.com>
2021-06-01 21:04:08 -0700

    Write better tests

--- src/test/kotlin/com/badger/lolbyte/utils/LaneRoleTest.kt
+++ src/test/kotlin/com/badger/lolbyte/utils/LaneRoleTest.kt
@@ -7,10 +7,8 @@ class LaneRoleTest {
     @Test
     fun testGetOrder() {
         Assertions.assertEquals(1, LaneRole.getOrder("BLAH", "BLAH"))
-        Assertions.assertEquals(1, LaneRole.getOrder("TOP", ""))
-        Assertions.assertEquals(2, LaneRole.getOrder("JUNGLE", ""))
-        Assertions.assertEquals(3, LaneRole.getOrder("MIDDLE", ""))
-        Assertions.assertEquals(4, LaneRole.getOrder("BOTTOM", "DUO_CARRY"))
-        Assertions.assertEquals(5, LaneRole.getOrder("BOTTOM", "DUO_SUPPORT"))
+        LaneRole.values().forEach { laneRole ->
+            Assertions.assertEquals(laneRole.order, LaneRole.getOrder(laneRole.lane, laneRole.role))
+        }
     }
 }
