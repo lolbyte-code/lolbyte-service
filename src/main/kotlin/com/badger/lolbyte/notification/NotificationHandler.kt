df4fae57a474e4510b963838274847304b8bd065
Anthony Schott <acschott526@gmail.com>
2021-06-05 09:59:44 -0700

    Allow empty alert

--- src/main/kotlin/com/badger/lolbyte/notification/NotificationHandler.kt
+++ src/main/kotlin/com/badger/lolbyte/notification/NotificationHandler.kt
@@ -1,9 +1,12 @@
 package com.badger.lolbyte.notification
 
+import com.badger.lolbyte.NotFoundException
+
 data class NotificationResponse(val alert: String)
 
 object NotificationHandler {
     fun getNotification(alert: String): NotificationResponse {
+        if (alert == "") throw NotFoundException
         return NotificationResponse(alert)
     }
 }
