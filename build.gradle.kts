cde912874298c54fce05ce4b54dd3f96a7572bc9
LolByte <crxlolbyte@gmail.com>
2023-12-27 11:24:56 -0800

    Bump R4J to 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.badger"
-version = "2.5.6"
+version = "2.5.7"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
@@ -23,7 +23,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
-    implementation("com.github.stelar7:R4J:2.2.22")
+    implementation("com.github.stelar7:R4J:2.4.0")
     implementation("com.merakianalytics.orianna:orianna:4.0.0-rc8")
     implementation("org.springframework.boot:spring-boot-starter-security:3.0.4")
 
