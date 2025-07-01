5f615be8f20cc6e5b526d03ca841da1714b48f53
LolByte <crxlolbyte@gmail.com>
2025-06-30 21:27:27 -0700

    Go back to remote version of R4J

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.badger"
-version = "2.11.1"
+version = "2.11.2"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
@@ -23,7 +23,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
-    implementation(files("lib/R4J-bcd26e63d7b5feebb871033f6354e8112dc7f8a2.jar"))
+    implementation("com.github.stelar7:R4J:2.6.0")
     implementation("org.springframework.boot:spring-boot-starter-security:3.0.4")
     implementation("org.cache2k:cache2k-core:2.6.1.Final")
 
